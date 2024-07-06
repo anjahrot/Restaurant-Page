29215eef46b5c3538c86d393d7a16c6ad70228b6
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-07-06 10:06:18 +0200

    Changed nav buttons to show selected once clicked, by changing opacity and set cursor to none

--- src/index.js
+++ src/index.js
@@ -10,11 +10,22 @@ const homeBtn = document.querySelector('#homeBtn');
 const menuBtn = document.querySelector('#menuBtn');
 const contactBtn = document.querySelector('#contactBtn');
 
+const buttons = [homeBtn, menuBtn, contactBtn];
+
 homeBtn.addEventListener('click', () => renderContent(initial_pageload));
 menuBtn.addEventListener('click',() => renderContent(getMenu));
 contactBtn.addEventListener('click', () => renderContent(getContact));
 
 function renderContent(createFunction){
+    buttons.forEach( btn => btn.classList.remove('chosen'));
     content.innerHTML = '';
+    if(createFunction === initial_pageload) {
+        homeBtn.classList.add('chosen');
+    }
+    else if(createFunction === getMenu) {
+        menuBtn.classList.add('chosen');
+    } else {
+        contactBtn.classList.add('chosen');
+    }
     createFunction();
 }
